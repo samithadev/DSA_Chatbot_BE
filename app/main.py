229f836b1a6add4098a6965eac213b0a7d5abf09
Samitha Wickramasinghe <contact.samitha@gmail.com>
2025-03-18 10:23:03 +0530

    fixed issue

--- app/main.py
+++ app/main.py
@@ -32,15 +32,15 @@ router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-origins = [
-    "http://localhost:5173",
-    "http://localhost:3000"
-    # "https://test.com"
-]
+# origins = [
+#     "http://localhost:5173",
+#     "http://localhost:3000"
+#     # "https://test.com"
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
